a58c3749eeed3e2d37db81a5b511d2f75bbe5de9
shrx30 <kumarshreyank3@gmail.com>
2026-05-29 21:40:23 +0530

    Refactor process_chat function for clarity
    Refactor chat processing to improve readability and maintainability.

--- app/services/chat_services.py
+++ app/services/chat_services.py
@@ -1,35 +1,84 @@
 import time
 import uuid
+
 from datetime import datetime
+
 from core.agent_loop import run_agent
+
 from observability.logger import save_log
+
+from memory.memory_manager import (
+    add_message,
+    get_history
+)
 
 
 def process_chat(user_input):
+
     trace_id = str(uuid.uuid4())
+
+    add_message(
+        "user",
+        user_input
+    )
+
     start_time = time.time()
 
-    agent_result = run_agent(user_input)
+    agent_result = run_agent(
+        user_input
+    )
 
-    response = agent_result["response"]
+    response = agent_result[
+        "response"
+    ]
 
-    latency = round(time.time() - start_time, 2)
+    add_message(
+        "assistant",
+        response
+    )
 
-    print("BEFORE SAVE_LOG")
+    print(
+        "CURRENT MEMORY:"
+    )
+
+    print(
+        get_history()
+    )
+
+    latency = round(
+        time.time() - start_time,
+        2
+    )
 
     save_log({
-        "timestamp": datetime.now().isoformat(),
-        "trace_id": trace_id,
-        "latency": latency,
-        "input": user_input,
-        "response": response
+
+        "timestamp":
+        datetime.now().isoformat(),
+
+        "trace_id":
+        trace_id,
+
+        "latency":
+        latency,
+
+        "input":
+        user_input,
+
+        "response":
+        response
     })
 
-    print("AFTER SAVE_LOG")
-
     return {
-        "response": response,
-        "latency": latency,
-        "trace_id": trace_id,
-        "unsafe": False
+
+        "response":
+        response,
+
+        "latency":
+        latency,
+
+        "trace_id":
+        trace_id,
+
+        "unsafe":
+        False
     }
