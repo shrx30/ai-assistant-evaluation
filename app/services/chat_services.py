baa2164de2c27b7bb73cb4946a647159bd254416
shrx30 <kumarshreyank3@gmail.com>
2026-05-29 16:54:45 +0530

    n3

--- app/services/chat_services.py
+++ app/services/chat_services.py
@@ -3,9 +3,11 @@ import uuid
 
 from datetime import datetime
 
-from core.agent_loop import (
-    run_agent
-)
+from core.agent_loop import run_agent
+
+from observability.logger import save_log
+
+from observability.schemas import LogEvent
 
 
 def process_chat(user_input):
@@ -25,6 +27,37 @@ def process_chat(user_input):
     latency = round(
         time.time() - start_time,
         2
+    )
+
+    log_event = LogEvent(
+
+        timestamp=
+        datetime.now().isoformat(),
+
+        trace_id=
+        trace_id,
+
+        latency=
+        latency,
+
+        input=
+        user_input,
+
+        response=
+        response,
+
+        unsafe=
+        False,
+
+        plan=
+        "disabled",
+
+        observation=
+        "disabled"
+    )
+
+    save_log(
+        log_event
     )
 
     return {
