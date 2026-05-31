0e1d2029eb7a233e32ca2ffdd9c14579bd2fc984
shrx30 <kumarshreyank3@gmail.com>
2026-05-31 14:44:19 +0530

    Update chat_services.py

--- app/services/chat_services.py
+++ app/services/chat_services.py
@@ -12,10 +12,61 @@ from memory.memory_manager import (
     get_history
 )
 
+from guardrails.guardrails import (
+    is_safe
+)
+
 
 def process_chat(user_input):
 
     trace_id = str(uuid.uuid4())
+
+    # -------------------------
+    # GUARDRAILS
+    # -------------------------
+
+    safe_request = is_safe(
+        user_input
+    )
+
+    if not safe_request:
+
+        save_log({
+
+            "timestamp":
+            datetime.now().isoformat(),
+
+            "trace_id":
+            trace_id,
+
+            "input":
+            user_input,
+
+            "response":
+            "Blocked by guardrails",
+
+            "safe_request":
+            False
+        })
+
+        return {
+
+            "response":
+            "Request blocked by guardrails.",
+
+            "latency":
+            0,
+
+            "trace_id":
+            trace_id,
+
+            "unsafe":
+            True
+        }
+
+    # -------------------------
+    # MEMORY
+    # -------------------------
 
     add_message(
         "user",
@@ -23,6 +74,10 @@ def process_chat(user_input):
     )
 
     start_time = time.time()
+
+    # -------------------------
+    # AGENT
+    # -------------------------
 
     agent_result = run_agent(
         user_input
@@ -50,6 +105,10 @@ def process_chat(user_input):
         2
     )
 
+    # -------------------------
+    # OBSERVABILITY
+    # -------------------------
+
     save_log({
 
         "timestamp":
@@ -71,7 +130,10 @@ def process_chat(user_input):
         plan,
 
         "observation":
-        observation
+        observation,
+
+        "safe_request":
+        True
     })
 
     return {
